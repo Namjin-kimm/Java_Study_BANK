10ccfe790ef2537252ece3531494d0cd0526eaad
Namjin-kimm <namjin-kim@daum.net>
2022-08-03 11:09:16 +0900

    Connection Test

--- src/main/java/com/iu/start/test/TestMain.java
+++ src/main/java/com/iu/start/test/TestMain.java
@@ -1,9 +1,21 @@
 package com.iu.start.test;
 
+import java.sql.Connection;
+
+import com.iu.start.util.DBConnector;
+
 public class TestMain {
 
 	public static void main(String[] args) {
-		System.out.println("Hello");
+//		System.out.println("Hello");
+		
+		try {
+			Connection con = DBConnector.getConnection();
+			System.out.println(con != null);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 
 	}
 
